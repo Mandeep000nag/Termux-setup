c10ce892c4704381a47ada8556ef2688b3419466
Mandeep@Nag.co.in <89760178+Mandeep000nag@users.noreply.github.com>
2021-09-16 08:31:40 +0530

    Add files via upload

---
+++ Termux-setup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+echo ""
+clear
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!       TOOL BY MANDEEP DHILLON         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+sleep 2
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[0m\e[1;96m!!! PACKAGE INSTALLATION !!! \e[0m'
+sleep 2
+clear
+echo ""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!         SABAR KARO DOWNLOAD          !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+termux-setup-storage
+pkg update -y 
+echo Sucess |lolcat
+pkg upgrade -y
+echo Sucess |lolcat
+sudo apt-get install apache2 -y
+echo Sucess |lolcat
+apt install 
+echo Sucess |lolcat
+apt install ruby -y
+echo Sucess |lolcat
+apt install php -y
+echo Sucess |lolcat
+apt install jq -y
+echo Sucess |lolcat
+apt install tail -y
+echo Sucess |lolcat
+apt install curl -y
+echo Sucess |lolcat
+apt install zip -y
+echo Sucess |lolcat
+pkg install wget -y
+echo Sucess |lolcat
+pkg install ruby -y
+echo Sucess |lolcat
+pkg install gem -y
+echo Sucess |lolcat
+gem install lolcat
+echo Sucess |lolcat
+clear
+chmod 7777 mandeep.sh
+clear
+echo ""
+echo -e $'\e[1;91m[\e[0m\e[1;33m *** \e[0m\e[1;96m]\e[0m\e[1;91m KEEP IN MIND YOU HAVE TO ENTER  \e[1;33m[ *** ]\e[0m'
+echo ""
+sleep2
+clear
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m !!    Installation Successfull   !!  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+sleep 4
